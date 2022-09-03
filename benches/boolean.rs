7a769517e7cf6e395a74d619993d3b7bf46980d5
Jihyun Yu <yjh0502@gmail.com>
2022-09-04 02:02:19 +0900

    bench: build_net

--- benches/boolean.rs
+++ benches/boolean.rs
@@ -9,7 +9,6 @@ pub fn criterion_benchmark(c: &mut Criterion) {
     let seed: <ChaCha20Rng as SeedableRng>::Seed = Default::default();
     let mut rng = ChaCha20Rng::from_seed(seed);
 
-    /*
     for subdivide in [1, 10, 100] {
         let p0 = points_cube_subdivide(Point::new([0.0, 0.0]), 100.0, subdivide);
         let p1 = points_cube_subdivide(Point::new([50.0, 50.0]), 100.0, subdivide);
@@ -24,7 +23,6 @@ pub fn criterion_benchmark(c: &mut Criterion) {
             |b| b.iter(|| s0.bool_intersect(&s1)),
         );
     }
-    */
 
     // complex visibility
     {
@@ -38,6 +36,8 @@ pub fn criterion_benchmark(c: &mut Criterion) {
             let sx_r = SimplicalChain::from_polygon(&p);
             sx = sx.bool_union(&sx_r);
         }
+
+        c.bench_function("build_net", |b| b.iter(|| build_net(view, &sx, true)));
 
         let net = build_net(view, &sx, true);
 
