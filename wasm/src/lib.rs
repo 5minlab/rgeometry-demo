50d09a70157ac804b4551ead001da1ec9b9f697f
Jihyun Yu <yjh0502@gmail.com>
2022-11-28 20:07:26 +0900

    wasm: visibility

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -110,17 +110,17 @@ impl Triangulated {
         triangulated
     }
 
-    pub fn visibility(&self, x: f64, y: f64) -> js_sys::Float32Array {
-        let vis = self.net.visibility(&self.constraints, &Point::new([x, y]));
-
-        visibility_serialize(&vis)
+    pub fn visibility(&self, x: f64, y: f64) -> Visibility {
+        let origin = Point::new([x, y]);
+        let vis = self.net.visibility(&self.constraints, &origin);
+        Visibility { origin, vis }
     }
 
-    pub fn visibility_limit(&self, x: f64, y: f64, limit: f64) -> js_sys::Float32Array {
-        let p = Point::new([x, y]);
-        let mut vis = self.net.visibility(&self.constraints, &p);
-        core::visibility_limit(&mut vis, &p, limit);
-        visibility_serialize(&vis)
+    pub fn visibility_limit(&self, x: f64, y: f64, limit: f64) -> Visibility {
+        let origin = Point::new([x, y]);
+        let mut vis = self.net.visibility(&self.constraints, &origin);
+        core::visibility_limit(&mut vis, &origin, limit);
+        Visibility { origin, vis }
     }
 
     pub fn visibility_limit_fill(
@@ -215,23 +215,98 @@ impl Triangulated {
 
         js_sys::Uint16Array::from(&v[..])
     }
+}
 
-    pub fn raycast(&self, origin_x: f64, origin_y: f64, coords: &[f64]) -> js_sys::Float32Array {
+#[wasm_bindgen]
+pub struct Visibility {
+    origin: Point<f64>,
+    vis: Vec<(Point<f64>, Point<f64>)>,
+}
+
+#[wasm_bindgen]
+impl Visibility {
+    pub fn serialize(&self) -> js_sys::Float32Array {
+        let mut v = Vec::with_capacity(self.vis.len() * 4);
+        for (from, to) in &self.vis {
+            v.push(from.array[0] as f32);
+            v.push(from.array[1] as f32);
+            v.push(to.array[0] as f32);
+            v.push(to.array[1] as f32);
+        }
+        js_sys::Float32Array::from(&v[..])
+    }
+
+    pub fn fill(&self, gridinfo: &[f64], grid: js_sys::Uint8Array) -> VisibilityResult {
+        let vis = &self.vis;
+        let mut count = 0;
+
+        if let [minx, miny, width, height, gridsize] = gridinfo {
+            let grid_count_x = (width / gridsize) as usize;
+            let grid_count_y = (height / gridsize) as usize;
+            let l = grid_count_x * grid_count_y;
+            let mut v = Vec::with_capacity(l);
+            v.resize(l, 0.0f32);
+
+            // find bounding box
+            let mut aabb: Option<AABB<f64>> = None;
+            for (p0, p1) in vis {
+                let aabb_other = core::raster::raster_bounds(
+                    *gridsize as usize,
+                    &[self.origin.clone(), p1.clone(), p0.clone()],
+                );
+                aabb = match aabb {
+                    Some(mut aabb) => {
+                        aabb.extend_aabb(&aabb_other);
+                        Some(aabb)
+                    }
+                    None => Some(aabb_other),
+                }
+            }
+
+            let aabb = aabb.unwrap_or(AABB::new(&Point::new([*minx, *miny])));
+
+            for (p0, p1) in vis {
+                core::raster::raster(
+                    *gridsize as usize,
+                    &[self.origin.clone(), p1.clone(), p0.clone()],
+                    |x, y| {
+                        let x = x - minx / gridsize;
+                        let y = y - miny / gridsize;
+                        if x < 0.0 || y < 0.0 {
+                            return;
+                        }
+                        count += 1;
+                        let x = x as usize;
+                        let y = y as usize;
+                        let idx = (y * grid_count_x + x) as u32;
+                        grid.set_index(idx, 1);
+                    },
+                )
+            }
+
+            VisibilityResult {
+                min_x: (aabb.min.array[0] - minx / gridsize).max(0.0),
+                min_y: (aabb.min.array[1] - miny / gridsize).max(0.0),
+                max_x: (aabb.max.array[0] - minx / gridsize).min(grid_count_x as f64),
+                max_y: (aabb.max.array[1] - miny / gridsize).min(grid_count_y as f64),
+                count,
+            }
+        } else {
+            todo!();
+        }
+    }
+
+    pub fn raycast(&self, coords: &[f64]) -> js_sys::Float32Array {
         let mut v = Vec::with_capacity(coords.len() * 2);
 
-        let p0 = Point::new([origin_x, origin_y]);
-
-        let vis = self.net.visibility(&self.constraints, &p0);
-        if vis.is_empty() {
-            return js_sys::Float32Array::from(&v[..]);
-        }
+        let p0 = &self.origin;
 
         for i in 0..coords.len() / 2 {
-            let x = coords[i * 2] + origin_x;
-            let y = coords[i * 2 + 1] + origin_y;
+            let x = coords[i * 2] + p0.array[0];
+            let y = coords[i * 2 + 1] + p0.array[1];
             let p1 = Point::new([x, y]);
 
-            let p2 = core::visibility::raycast(&p0, &vis, &p1).unwrap_or(p0);
+            let p2 = core::visibility::raycast(p0, &self.vis, &p1).unwrap_or_else(|| p0.clone());
             v.push(p2.array[0] as f32);
             v.push(p2.array[1] as f32);
         }
@@ -323,15 +398,4 @@ pub struct VisibilityResult {
     pub max_y: f64,
 
     pub count: usize,
-}
-
-fn visibility_serialize(vis: &[(Point<f64>, Point<f64>)]) -> js_sys::Float32Array {
-    let mut v = Vec::with_capacity(vis.len() * 4);
-    for (from, to) in vis {
-        v.push(from.array[0] as f32);
-        v.push(from.array[1] as f32);
-        v.push(to.array[0] as f32);
-        v.push(to.array[1] as f32);
-    }
-    js_sys::Float32Array::from(&v[..])
 }
